4752f73cf233eed75865bc56160c78c9fb90a85a
kdh92417 <kdh92417@gmail.com>
2022-09-06 23:35:16 +0900

    Feat: posting Apps 추가
    - INSTALLED_APPS에 posting App 추가

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -41,14 +41,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
     # DRF
     "rest_framework",
+    # Apps
+    "posting",
 ]
 
 MIDDLEWARE = [
