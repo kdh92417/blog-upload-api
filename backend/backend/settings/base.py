b3fea47873ab46f75d2afb46d2ed53471629a040
kdh92417 <kdh92417@gmail.com>
2022-09-06 11:49:46 +0900

    :memo: Fix: develop 브랜치 최신화
    develop 브랜치 최신화 및 머지

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -41,15 +41,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # DRF
-    'rest_framework',
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
