bd526a24e2a834307ce68a9408d47d1811e04f33
kdh92417 <kdh92417@gmail.com>
2022-09-07 13:32:02 +0900

    Feat: 포스팅 모델 수정
    생성시간과 수정시간 컬럼 추가

--- backend/posting/models.py
+++ backend/posting/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
 
-# 포스팅 모델
 class Posting(models.Model):
+    """
+    포스팅 모델
+    """
     title = models.CharField(max_length=20)
     content = models.CharField(max_length=200)
     password = models.CharField(max_length=60)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = "postings"
